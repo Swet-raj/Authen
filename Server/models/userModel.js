f67bee02736ef6c5e0ec3313c8a83cf7b645b6b4
Swet-raj <swetraj20042005@gmail.com>
2026-03-14 00:25:43 +0530

    c

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -4,9 +4,9 @@ const userSchema=new mongoose.Schema({
     name: {type: String,required: true},
     email: {type: String,required: true,unique: true},
     password:{ type:String, required:true},
-    verifyotp:{type:String,default:''},
-    verifyotpexpiresat:{type:Number,default:0},
-    isaccountverified:{type:Boolean,default:false},
+    verifyOtp:{type:String,default:''},
+    verifyOtpExpiresAt:{type:Number,default:0},
+    isAccountVerified:{type:Boolean,default:false},
     resetotp:{ type:String,default:''},
     resetotpexpiresat:{type:Number,default:0},
 })
