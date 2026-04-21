f7677faef7c3df15baba7a1b6f63ca42dbb01a24
Swet-raj <swetraj20042005@gmail.com>
2026-04-21 19:03:35 +0530

    Final

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -2,11 +2,42 @@ import React, { useContext } from 'react'
 import { assets } from './../assets/assets';
 import { useNavigate } from 'react-router-dom';
 import { AppContent } from '../context/AppContext';
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const Navbar = () => {
 
   const navigate = useNavigate()
-  const { userData, backendUrl, setUserData, setLoggedin} = useContext(AppContent)
+  const { userData, backendUrl, setUserData, setIsLoggedin} = useContext(AppContent)
+
+  const sendVerificationOtp = async ()=>{
+    try {
+      axios.defaults.withCredentials = true;
+
+      const { data } = await axios.post(backendUrl + '/api/auth/send-verify-otp')
+
+      if(data.success) {
+        navigate('/email-verify')
+        toast.success(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const logout = async ()=>{
+    try {
+      axios.defaults.withCredentials = true
+      const {data } = await axios.post(backendUrl + '/api/auth/logout')
+
+      data.success && setIsLoggedin(false)
+      data.success && setUserData(false)
+      navigate('/')
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   
   return (
@@ -18,8 +49,9 @@ const Navbar = () => {
         {userData.name[0].toUpperCase()}
         <div className= 'absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
           <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-            <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>
-            <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
+            {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>}
+            
+            <li onClick= {logout}className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
           </ul>
 
         </div>
--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -5,21 +5,22 @@ import { toast } from "react-toastify";
 export const AppContent = createContext();
 
 export const AppContextProvider = (props) => {
+
+    axios.defaults.withCredentials = true;
+
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    console.log(backendUrl);
     const [isLoggedin, setIsLoggedin] = useState(false);
     const [userData, setUserData] = useState(false);
 
-
     const getAuthState = async()=> {
         try {
-            const { data } =await axios.get(`${backendUrl}/api/user/is-auth`)
+            const { data } = await axios.get(`${backendUrl}/api/auth/is-auth`)
             if(data.success) {
                 setIsLoggedin(true)
                 getUserData()
             }
         } catch (error) {
-            toast.error(data.message || "Unable to fetch user data");
+            toast.error(error.response?.data?.message || error.message || "Unable to fetch user data");
         }
     }
 
@@ -41,7 +42,7 @@ export const AppContextProvider = (props) => {
 
     useEffect(()=>{
         getAuthState();
-    })
+    },[])
 
     const value = {
         backendUrl,
